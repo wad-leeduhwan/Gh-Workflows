4df71ec93601ad194697d0adee8d370c22512cef
GitHub Action <action@github.com>
2026-03-03 01:01:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/wadleeduhwan/ghworkflows/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/wadleeduhwan/ghworkflows/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.wadleeduhwan.ghworkflows.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wadleeduhwan.ghworkflows.MyBundle
+import com.github.wadleeduhwan.ghworkflows.services.MyProjectService
 import javax.swing.JButton
 
 
